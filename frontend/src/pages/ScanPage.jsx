f74bb9eb8660eabc3eaca5730e133e2d4c5f9132
najiya <najiyakk11@gmail.com>
2026-02-28 11:00:29 +0530

    Create ScanPage and connect to App

---
+++ frontend/src/pages/ScanPage.jsx
@@ -0,0 +1,11 @@
+function ScanPage() {
+  return (
+    <div>
+      <h2>Upload Product Label</h2>
+      <input type="file" />
+      <button>Analyze Ingredients</button>
+    </div>
+  )
+}
+
+export default ScanPage
